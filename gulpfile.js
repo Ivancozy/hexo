1ab272b735973d2e98f60c199e8bb58ebdb4770c
Ivancozy <yf1482253165@gmail.com>
2022-11-19 23:22:43 +0800

    pwa-all

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,3 @@
-/*
- * @Description: gulp
- * @Author: 安知鱼
- * @Email: 2268025923@qq.com
- * @Date: 2022-02-22 11:22:57
- * @LastEditTime: 2022-08-18 12:24:11
- * @LastEditors: 安知鱼
- */
 import gulp from "gulp";
 import cleanCSS from "gulp-clean-css";
 import htmlmin from "gulp-htmlmin";
--- sw-template.js
+++ sw-template.js
@@ -1,11 +1,3 @@
-/*
- * @Description: sw
- * @Author: 安知鱼
- * @Email: 2268025923@qq.com
- * @Date: 2022-02-22 11:23:58
- * @LastEditTime: 2022-03-08 12:24:30
- * @LastEditors: 安知鱼
- */
 const workboxVersion = "5.1.3";
 
 importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
