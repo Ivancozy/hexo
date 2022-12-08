5dda71142e116fd0658024a5a9d85b5ac80b2eda
Ivancozy <61648574+Ivancozy@users.noreply.github.com>
2022-12-08 18:22:16 +0000

    🎉 INT COMMIT: fix UI Updated By Github Actions

--- js/anzhiyufunction.js
+++ js/anzhiyufunction.js
@@ -1,1 +1,1 @@
-function musicBindEvent(){document.querySelector("#nav-music .aplayer-music").addEventListener("click",(function(){anzhiyu.musicTelescopic()}))}
+function musicBindEvent(){document.querySelector("#nav-music .aplayer-music").addEventListener("click",(function(){anzhiyu.musicTelescopic()})),document.querySelector("#nav-music .aplayer-button").addEventListener("click",(function(){anzhiyu.musicToggle(!1)}))}
